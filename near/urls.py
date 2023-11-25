b4349a322dde6691506f280f7870b43734a1d307
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-11-25 19:18:38 +0200

    Add OpenAPI

--- near/urls.py
+++ near/urls.py
@@ -2,4 +2,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+from .schema import schema_view
+
+urlpatterns = [
+    path(
+        "schema/",
+        schema_view.with_ui(),
+        name="schema",
+    ),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
