06e843f75cfaed272e925f05ea59389c940de697
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-12-22 22:13:55 +0200

    Add Media endpoints

--- near/urls.py
+++ near/urls.py
@@ -1,11 +1,18 @@
-from django.urls import path
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+
+from media.viewsets import MediaViewSet
 
 from .schema import schema_view
 
+router = DefaultRouter()
+router.register("media", MediaViewSet, basename="media")
+
 urlpatterns = (
     [
+        path("", include(router.urls)),
         path(
             "schema/",
             schema_view.with_ui(),
