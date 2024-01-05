ca28b13fb2120e05e27278f33d53708860cb2901
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2024-01-05 13:30:51 +0200

    Provide email settings

--- near/settings.py
+++ near/settings.py
@@ -129,3 +129,12 @@ SIMPLE_JWT = {
     "UPDATE_LAST_LOGIN": True,
     "AUTH_HEADER_TYPES": ["Token", "Bearer"],
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS")
+EMAIL_PORT = env.int("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = env("EMAIL_HOST_USER")
