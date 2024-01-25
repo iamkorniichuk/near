0141787e4461d7c18ceb7e0976dcd7f0f3993e8f
iamkorniichuk <iamkorniichuk@gmail.com>
2024-01-25 11:59:22 +0200

    Fix updating autopopulated fields

--- profiles/views.py
+++ profiles/views.py
@@ -2,24 +2,25 @@ from rest_framework import generics
 from rest_framework import permissions
 from rest_framework.parsers import MultiPartParser, FormParser
 
+from commons.mixins import PopulateCreateDataMixin, PopulateUpdateDataMixin
+
 from .serializers import ProfileSerializer
 from .models import Profile
 from .permissions import HasProfile
 
 
-class ProfileListView(generics.ListCreateAPIView):
+class ProfileListView(PopulateCreateDataMixin, generics.ListCreateAPIView):
     parser_classes = [MultiPartParser, FormParser]
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
-
-    def create(self, request, *args, **kwargs):
-        request.data["user"] = self.request.user.pk
-        return super().create(request, *args, **kwargs)
 
     def get_permissions(self):
         if self.request.method == "POST":
             return [permissions.AllowAny()]
         return super().get_permissions()
+
+    def get_populated_data(self):
+        return {"user": self.request.user.pk}
 
 
 class ProfileDetailsView(generics.RetrieveAPIView):
@@ -27,7 +28,7 @@ class ProfileDetailsView(generics.RetrieveAPIView):
     queryset = Profile.objects.all()
 
 
-class MyProfileDetailsView(generics.RetrieveUpdateAPIView):
+class MyProfileDetailsView(PopulateUpdateDataMixin, generics.RetrieveUpdateAPIView):
     parser_classes = [MultiPartParser, FormParser]
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
@@ -35,3 +36,6 @@ class MyProfileDetailsView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user.profile
+
+    def get_populated_data(self):
+        return {"user": self.request.user.pk}
