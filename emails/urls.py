10caf7ad7e9851f52232fe6d6abc0bb4b4307d91
iamkorniichuk <119371190+iamkorniichuk@users.noreply.github.com>
2024-02-04 21:28:36 +0200

    Revert "Initial development release (#28)"
    This reverts commit 5df0f7653b006fe351e251683c5a8376791b20a0.

--- emails/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import VerifyEmailView
-
-app_name = "emails"
-
-urlpatterns = [
-    path("verify-email/", VerifyEmailView.as_view(), name="verify-email"),
-]
