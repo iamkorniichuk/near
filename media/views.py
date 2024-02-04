f5bc2d3edb00ff82382c2287c65a8c57283dd8a1
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2024-02-04 20:44:22 +0200

    Change Media views to resolve content_type by model name

--- media/views.py
+++ media/views.py
@@ -1,11 +1,14 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.parsers import MultiPartParser, FormParser
+from django.contrib.contenttypes.models import ContentType
+
+from commons.mixins import PopulateCreateDataMixin, PopulateUpdateDataMixin
 
 from .serializers import MediaSerializer, MultipleMediaSerializer
 from .models import Media
 
 
-class MediaListView(ListCreateAPIView):
+class MediaListView(PopulateCreateDataMixin, ListCreateAPIView):
     parser_classes = [MultiPartParser, FormParser]
     queryset = Media.objects.all()
 
@@ -14,8 +17,19 @@ class MediaListView(ListCreateAPIView):
             return MultipleMediaSerializer
         return MediaSerializer
 
+    def get_populated_data(self, *args, **kwargs):
+        content_type = ContentType.objects.get(model=self.request.data["model"])
+        return {"content_type": content_type.pk}
 
-class MediaDetailsView(RetrieveUpdateDestroyAPIView):
+
+class MediaDetailsView(PopulateUpdateDataMixin, RetrieveUpdateDestroyAPIView):
     parser_classes = [MultiPartParser, FormParser]
     serializer_class = MediaSerializer
     queryset = Media.objects.all()
+
+    def get_populated_data(self, *args, **kwargs):
+        model = self.request.data.get("model", None)
+        if model:
+            content_type = ContentType.objects.get(model=model)
+            return {"content_type": content_type.pk}
+        return {}
